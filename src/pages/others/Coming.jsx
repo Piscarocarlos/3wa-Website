f15b31a1ea511ac56ae25d09326d58e752744b77
Carlos Alognon <carlosalognon08@gmail.com>
2022-11-24 13:08:07 +0100

    Add coming soon page

---
+++ src/pages/others/Coming.jsx
@@ -0,0 +1,100 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useLoadingContext } from "react-router-loading";
+import LogoColor from "../../assets/images/logo-color.png"
+import Shape1 from "../../assets/images/shapes/shape-11-06.png"
+import Shape2 from "../../assets/images/shapes/shape-09-02.png"
+import Shape3 from "../../assets/images/shapes/shape-03-02.png"
+import Shape4 from "../../assets/images/shapes/shape-14-04.png"
+import Shape5 from "../../assets/images/shapes/shape-03-08.png"
+import Shape6 from "../../assets/images/shapes/shape-01-02.png"
+
+export default function Coming() {
+
+  const loadingContext = useLoadingContext();
+
+  const loading = async () => {
+    loadingContext.done();
+  };
+  return (
+   <>
+     <div className="main-wrapper">
+      <div className="eduvibe-coming-soon edu-coming-soon-area edu-coming-soon-style ptb--150 ptb_md--80 ptb_sm--80 bg-image">
+        <div className="container eduvibe-animated-shape">
+          <div className="row g-5">
+            <div className="col-xl-10 offset-xl-1">
+              <div className="content text-center">
+                <div className="logo">
+                  <Link to="/test">
+                    <img src={LogoColor} alt="" className="logo-size" />
+                  </Link>
+                  <h1 className="title">We Are Making <br /> Something Specials</h1>
+                  <div className="countdown-style-2">
+                    <div className="countdown" data-date="2022-12-30">
+                      <div className="countdown-container days">
+                        <span className="countdown-value">25</span>
+                        <span className="countdown-heading">Days</span>
+                      </div>
+                      <div className="countdown-container hours">
+                        <span className="countdown-value">23</span>
+                        <span className="countdown-heading">Hours</span>
+                      </div>
+                      <div className="countdown-container minutes">
+                        <span className="countdown-value">38</span>
+                        <span className="countdown-heading">Minutes</span>
+                      </div>
+                      <div className="countdown-container seconds">
+                        <span className="countdown-value">27</span>
+                        <span className="countdown-heading">Seconds</span>
+                      </div>
+                    </div>
+                  </div>
+                  <p className="description">
+                    <span className="fw-bold">3W Academy</span> est un 
+                    <span className="fw-bold">bootcamp</span> 
+                    accélérateur de carrières dans les métiers du <span className="fw-bold">développement WEB et Mobile</span> à travers des formations courtes, intensives et <span className="fw-bold">90% pratiques</span>.
+                    </p>
+                    <p className="description">
+                    Au Maroc, +450 lauréats ont réussi cette formation et 87% décrochent un job ou à lancent leurs propres projets moins de 6 mois après leur certification, pourquoi pas toi ?
+                    </p>
+                    <div className="newsletter-style-6">
+                        <form className="newsletter-form-style-2" action="#">
+                            {/* <input type="email" placeholder="Enter your mail address"/> */}
+                            <button className="edu-btn w-75">Rejoindre une formation <i className="icon-arrow-right-line-right"></i></button>
+                        </form>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="shape-dot-wrapper shape-wrapper d-xl-block d-none">
+            <div className="shape-image shape-image-1">
+              <img src={Shape1} alt="Shape Thumb" />
+            </div>
+            <div className="shape-image shape-image-2">
+              <img src={Shape2} alt="Shape Thumb" />
+            </div>
+            <div className="shape-image shape-image-3">
+              <img src={Shape3} alt="Shape Thumb" />
+            </div>
+            <div className="shape-image shape-image-4">
+              <img src={Shape4} alt="Shape Thumb" />
+            </div>
+            <div className="shape-image shape-image-5">
+              <img src={Shape5} alt="Shape Thumb" />
+            </div>
+            <div className="shape-image shape-image-6">
+              <img src={Shape6} alt="Shape Thumb" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+   </>
+  )
+}
+
+
+
